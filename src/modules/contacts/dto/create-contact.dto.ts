e33359e02cb1a2574b3922aef5039e02ae21a7fc
= <achten.rafael16@gmail.com>
2023-12-04 16:29:28 -0300

    feat: add  global ValidationPipe and new validation in CreateContactDto/CreateCategoryDto with class-validator

--- src/modules/contacts/dto/create-contact.dto.ts
+++ src/modules/contacts/dto/create-contact.dto.ts
@@ -1,6 +1,19 @@
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
 export class CreateContactDto {
+  @IsString()
+  @IsNotEmpty()
   categoryId: string;
+
+  @IsString()
+  @IsNotEmpty()
   name: string;
+
+  @IsEmail()
+  @IsNotEmpty()
   email: string;
+
+  @IsString()
+  @IsNotEmpty()
   phone: string;
 }
